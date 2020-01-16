c9ebcc0fa36c571c5b51171e37d4abcd3c9e6d92
Victoria Curl <victoriacurl9@gmail.com>
2020-01-16 19:21:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -16,6 +16,22 @@ def color
   @color
 end
 
+def size=(size)
+  @size = size
+end
+
+def size
+  @size
+end
+
+def material=(material)
+  @material = material
+end
+
+def material
+  @material
+end
+
 
 
 end

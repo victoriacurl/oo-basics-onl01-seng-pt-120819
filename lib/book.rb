b5f1d8ebea1d4c22282d2c25d3be81018a9d7458
Victoria Curl <victoriacurl9@gmail.com>
2020-01-16 18:23:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,6 +11,18 @@ def author=(author)
   @author = author
 end
 
+def author
+  @author
+end
+
+def page_count=(num)
+  @page_count = num
+end
+
+def page_count
+  @page_count
+end
+
 end
 
 Book.new("And Then There Were None")
